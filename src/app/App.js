01cc6e6eff57b03c0fbafee69c18d2b7f59bc92b
Andy <amillion3@gmail.com>
2018-07-12 18:31:29 -0500

    WIP, working on logout functionality

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Login from '../components/Login/Login';
 import Register from '../components/Register/Register';
+import Navbar from '../components/Navbar/Navbar';
 
 import fbConnection from '../firebaseRequests/connection';
 
@@ -13,6 +14,7 @@ class App extends Component {
   render () {
     return (
       <div className="App">
+        <Navbar />
         <Register />
         <Login />
       </div>
